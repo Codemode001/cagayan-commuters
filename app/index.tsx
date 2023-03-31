999c1f6a733734bbbbcd5db154d7a3d48af1b75e
Harold <haroldrex12331@gmail.com>
2023-03-31 22:53:59 +0800

    starting onboarding

--- app/index.tsx
+++ app/index.tsx
@@ -12,19 +12,11 @@ import data from "../data/welcomeData.json";
 import { styles } from "../constants/theme";
 import Onboarding from "../components/Onboarding";
 
-const renderItem = ({ item }: any) => {
-  return (
-    <View>
-      <Text>{item.title}</Text>
-    </View>
-  );
-};
-
 const Home = () => {
   const { width } = useWindowDimensions();
 
   return (
-    <View style={[styles.container, { backgroundColor: "gray" }]}>
+    <View style={[styles.container]}>
       <Stack.Screen
         options={{
           headerShown: false,
@@ -34,6 +26,11 @@ const Home = () => {
       <FlatList
         data={data.welcome}
         renderItem={({ item }) => <Onboarding item={item} />}
+        horizontal
+        showsHorizontalScrollIndicator
+        pagingEnabled
+        bounces={false}
+        scrollEventThrottle={32}
       />
     </View>
   );
--- components/Onboarding.tsx
+++ components/Onboarding.tsx
@@ -1,7 +1,12 @@
 import React from "react";
-import { Text, View, useWindowDimensions, Image } from "react-native";
-
-import { styles } from "../constants/theme";
+import {
+  Text,
+  View,
+  useWindowDimensions,
+  Image,
+  StyleSheet,
+  StatusBar,
+} from "react-native";
 
 interface ItemProps {
   id: number;
@@ -11,13 +16,54 @@ interface ItemProps {
 }
 
 const Onboarding = ({ item }: { item: ItemProps }) => {
-  const { width } = useWindowDimensions();
+  const { width, height } = useWindowDimensions();
+
+  const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      alignItems: "center",
+      justifyContent: "center",
+    },
+    image: {
+      position: "absolute",
+      top: 0,
+      left: 0,
+      right: 0,
+      bottom: 0,
+    },
+    textContainer: {
+      position: "absolute",
+      top: "50%",
+      left: "50%",
+      transform: [{ translateX: -width / 2 }, { translateY: -height / 4 }],
+      backgroundColor: "rgba(0,0,0,0.5)",
+      padding: 20,
+      alignItems: "center",
+      justifyContent: "center",
+    },
+    title: {
+      fontSize: 30,
+      fontWeight: "bold",
+      color: "#fff",
+    },
+    description: {
+      fontSize: 16,
+      color: "#fff",
+      textAlign: "center",
+    },
+  });
 
   return (
     <View style={[styles.container, { width }]}>
+      <StatusBar
+        backgroundColor="#000000"
+        translucent
+        barStyle="light-content"
+      />
+
       <Image
-        source={item.image}
-        style={[styles.image, { width, resizeMode: "contain" }]}
+        source={{ uri: item.image }}
+        style={[styles.image, { width, height }]}
       />
       <View style={{ flex: 0.3 }}>
         <Text style={styles.title}>{item.title}</Text>
